825f5b0d577ff44032615678a5121bb4583d99b5
Jan-Erik Rediger <janerik@fnordig.de>
2016-07-17 14:35:48 +0200

    feat: Implement join-with-timeout

--- src/lib.rs
+++ src/lib.rs
@@ -26,6 +26,21 @@
 //! let t = thread::spawn(|| { thread::sleep(Duration::from_secs(1)); });
 //! assert!(t.try_join().is_err());
 //! ```
+//!
+//! To perform a join-with-timeout there is a `try_timed_join` method.
+//!
+//! # Example join-with-timeout
+//!
+//! ```rust
+//! # use std::time::Duration;
+//! # use std::thread;
+//! use thread_tryjoin::TryJoinHandle;
+//!
+//! let t = thread::spawn(|| {
+//!     thread::sleep(Duration::from_millis(200));
+//! });
+//! assert!(t.try_timed_join(Duration::from_millis(500)).is_ok());
+//! ```
 #![deny(missing_docs)]
 
 extern crate libc;
@@ -33,16 +48,27 @@ extern crate libc;
 use std::{thread, ptr};
 use std::os::unix::thread::JoinHandleExt;
 use std::io::Error as IoError;
+use std::time::{self, Duration, SystemTime};
 
 #[cfg(unix)]
 extern "C" {
     fn pthread_tryjoin_np(thread: libc::pthread_t, retval: *mut *mut libc::c_void) -> libc::c_int;
+    fn pthread_timedjoin_np(thread: libc::pthread_t,
+                            retval: *mut *mut libc::c_void,
+                            abstime: *const libc::timespec) -> libc::c_int;
 }
 
-/// Try joining a thread
+/// Try joining a thread.
 pub trait TryJoinHandle {
-    /// Try joining a thread
+    /// Try joining a thread.
     fn try_join(&self) -> Result<(), IoError>;
+
+    /// Try joining a thread with a timeout.
+    ///
+    /// This waits for the specified duration.
+    /// If the timeout expires before the thread terminates, the call returns an error.
+    /// Otherwise it succeeds.
+    fn try_timed_join(&self, wait: Duration) -> Result<(), IoError>;
 }
 
 #[cfg(unix)]
@@ -57,11 +83,33 @@ impl<T> TryJoinHandle for thread::JoinHandle<T> {
             }
         }
     }
+    fn try_timed_join(&self, wait: Duration) -> Result<(), IoError> {
+        unsafe {
+            let thread = self.as_pthread_t();
+
+            let now = SystemTime::now();
+            let future = now + wait;
+            let total = future.duration_since(time::UNIX_EPOCH).expect("Can't get time offset");
+            let abstime = libc::timespec {
+                tv_sec: total.as_secs() as i64,
+                tv_nsec: total.subsec_nanos() as i64
+            };
+
+            match pthread_timedjoin_np(thread, ptr::null_mut(), &abstime as *const libc::timespec) {
+                0 => Ok(()),
+                err @ _ => Err(IoError::from_raw_os_error(err))
+            }
+        }
+    }
 }
 
 #[cfg(not(unix))]
 impl<T> TryJoinHandle for thread::JoinHandle<T> {
     fn try_join(&self) -> Result<(), IoError> {
+        Err(IoError::from_raw_os_error(2))
+    }
+
+    fn try_timed_join(&self, _wait: Duration) -> Result<(), IoError> {
         Err(IoError::from_raw_os_error(2))
     }
 }
@@ -100,5 +148,23 @@ mod test {
         thread::sleep(Duration::from_secs(1));
 
         assert!(t.try_join().is_ok());
+    }
+
+    #[test]
+    fn basic_timed_join() {
+        let t = thread::spawn(|| { "ok" });
+        assert!(t.try_timed_join(Duration::from_secs(1)).is_ok());
+    }
+
+    #[test]
+    fn timed_join_timeout() {
+        let t = thread::spawn(|| { thread::sleep(Duration::from_millis(500)); });
+        assert!(t.try_timed_join(Duration::from_millis(100)).is_err());
+    }
+
+    #[test]
+    fn timed_join_works() {
+        let t = thread::spawn(|| { thread::sleep(Duration::from_millis(100)); });
+        assert!(t.try_timed_join(Duration::from_millis(500)).is_ok());
     }
 }
